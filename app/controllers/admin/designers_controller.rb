5cf9297aa5a3486e41471d3c034eb806c16cf27a
vitor <vitor@vitor-VirtualBox.(none)>
2013-10-14 15:22:06 -0300

    Admin/designers

--- app/controllers/admin/designers_controller.rb
+++ app/controllers/admin/designers_controller.rb
@@ -1,2 +1,49 @@
-class Admin::DesignersController < ApplicationController
+class Admin::DesignersController < Admin::AdminController
+  before_filter :authenticate
+  
+  def index
+    @designers = Designer.all
+  end
+  
+  def new
+    @designer = Designer.new
+    
+    respond_to do |format|
+      format.html #new.html.erb
+    end
+  end
+  
+  def create
+    @designer = Designer.create( params[:designer] )
+    
+    respond_to do |format|
+      if @designer.save
+        format.html { redirect_to(:action => "index") }
+      else 
+        format.html { redirect_to(:action => "new") }
+      end
+    end
+  end
+  
+  def destroy
+    designer = Designer.find( params[:id] )
+    designer.delete
+    
+    respond_to do |format|
+      format.html { redirect_to(:action => "index") }
+    end
+  end
+  
+  def edit
+    @designer = Designer.find( params[:id] )
+  end
+  
+  def update
+    @designer = Designer.find( params[:id] )
+    @designer.update_attributes( params[:designer] )
+    
+    respond_to do |format|
+      format.html { redirect_to(:action => "index") }
+    end
+  end
 end
--- app/models/designer.rb
+++ app/models/designer.rb
@@ -2,12 +2,14 @@ class Designer < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
   
-  mount_uploader :picture, ProfilePictureUploader
+  mount_uploader :image, ProfilePictureUploader
   
   default_scope :order => "name ASC"
   
   translates :about
   accepts_nested_attributes_for :translations
+  
+  attr_accessible :name, :surname, :about, :facebook, :blogger, :behance, :pinterest, :vimeo, :linkedin, :email, :image, :translations_attributes
   
   has_and_belongs_to_many :jobs
   has_many :for_sale_items
